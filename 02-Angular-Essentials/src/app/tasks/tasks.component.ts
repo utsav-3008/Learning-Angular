79b7c5ffb29dcd7fe82f07d1f4b07ed1081dbda4
utsav-3008 <40323216+utsav-3008@users.noreply.github.com>
2026-08-26 18:21:28 +0530

    Add and Complete Tasks,Service ts layer, LocalStorage

--- 02-Angular-Essentials/src/app/tasks/tasks.component.ts
+++ 02-Angular-Essentials/src/app/tasks/tasks.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -12,34 +15,24 @@ export class TasksComponent {
   
   @Input({required: true}) userId!: string
   @Input({required: true}) name!: string;
-  tasks = [
-  {
-    id: 't1',
-    userId: 'u1',
-    title: 'Master Angular',
-    summary:
-      'Learn all the basic and advanced features of Angular & how to apply them.',
-    dueDate: '2025-12-31',
-  },
-  {
-    id: 't2',
-    userId: 'u3',
-    title: 'Build first prototype',
-    summary: 'Build a first prototype of the online shop website',
-    dueDate: '2024-05-31',
-  },
-  {
-    id: 't3',
-    userId: 'u3',
-    title: 'Prepare issue template',
-    summary:
-      'Prepare and describe an issue template which will help with project management',
-    dueDate: '2024-06-15',
-  },
-];
+  isAddingTask = false;
+  
+  constructor(private tasksService: TasksService) {}
 
-get selectedUserTasks() {
-  return this.tasks.filter((task) => task.userId === this.userId);
-}
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
 
 }
