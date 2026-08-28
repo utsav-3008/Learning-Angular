eb426d85cfa6bfe1569ef753bcb4f033c4f9e408
utsav-3008 <40323216+utsav-3008@users.noreply.github.com>
2026-08-28 17:53:58 +0530

    Same App but with using NgModules

--- 03-Angular-With-Modules/src/app/app.module.ts
+++ 03-Angular-With-Modules/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';;
+
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
