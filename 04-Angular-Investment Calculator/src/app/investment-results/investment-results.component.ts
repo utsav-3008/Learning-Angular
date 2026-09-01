900c64b1aa1bd57cd3951545bcbc93de5efe8738
utsav-3008 <40323216+utsav-3008@users.noreply.github.com>
2026-09-01 17:09:13 +0530

    Converting the app into module based angular app

--- 04-Angular-Investment Calculator/src/app/investment-results/investment-results.component.ts
+++ 04-Angular-Investment Calculator/src/app/investment-results/investment-results.component.ts
@@ -1,21 +1,16 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, Input, input } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  // results = input()
-  @Input() results?: {
-        year: number,
-        interest: number,
-        valueEndOfYear: number,
-        annualInvestment: number,
-        totalInterest: number,
-        totalAmountInvested: number,
-      }[];
+  private investmentService = inject(InvestmentService);
+
+  results = computed(() => {
+    return this.investmentService.resultData();
+  });
 }
