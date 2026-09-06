92ead9b19f68e5d8e58b8778d2076be69311a96e
utsav-3008 <40323216+utsav-3008@users.noreply.github.com>
2026-09-06 18:19:53 +0530

    encapsulation to remove wrapper tags

--- 06-Component_Deep-Dive/src/app/shared/control/control.component.ts
+++ 06-Component_Deep-Dive/src/app/shared/control/control.component.ts
@@ -1,12 +1,26 @@
-import { Component, input } from '@angular/core';
+import { Component, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //  console.log('Control clicked!');
+  // }
   label = input.required<string>();
+  
+  onClick() {
+    console.log('Control clicked!');
+  }
+
 }
