cfb63187c00d18a475b144f465ab45e6433179c3
utsav-3008 <40323216+utsav-3008@users.noreply.github.com>
2026-09-06 10:51:11 +0530

    Refactoring CSS, Making components modular

--- 06-Component_Deep-Dive/src/app/shared/button/button.component.ts
+++ 06-Component_Deep-Dive/src/app/shared/button/button.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+
 
 }
